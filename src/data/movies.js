184b0e49da1ceb74905fae00b400f62c990b734c
Davi-D18 <luiz.araujo@alunomicrolins.com.br>
2025-01-09 17:11:57 -0300

    feat(custom): novo componente \"cardFilme\"
    esse componente exibe uma imagem de um filme junto com uma barra de progresso
    
    Arquivos alterados:
    src/assets/images/filmes/filme-1.png
    src/assets/images/filmes/filme-2.png
    src/assets/images/filmes/filme-3.png
    src/assets/images/filmes/filme-4.png
    src/assets/images/filmes/filme-5.png
    src/components/cardFilme/CardFilme.jsx
    src/components/cardFilme/style/cardfilme.module.scss
    src/data/movies.js

---
+++ src/data/movies.js
@@ -0,0 +1,33 @@
+import filme_1 from '@assets/images/filmes/filme-1.png'
+import filme_2 from '@assets/images/filmes/filme-2.png'
+import filme_3 from '@assets/images/filmes/filme-3.png'
+import filme_4 from '@assets/images/filmes/filme-4.png'
+import filme_5 from '@assets/images/filmes/filme-5.png'
+
+export const filmes = [
+  {
+    id: 1,
+    image: filme_1,
+    progress: 80
+  },
+  {
+    id: 2,
+    image: filme_2,
+    progress: 80
+  },
+  {
+    id: 3,
+    image: filme_3,
+    progress: 75
+  },
+  {
+    id: 4,
+    image: filme_4,
+    progress: 70
+  },
+  {
+    id: 5,
+    image: filme_5,
+    progress: 100
+  }
+]
